8ef200933a978dbaf66c5082757eff13fc290f11
kote1999 <kotekirkitadze@gmail.com>
2021-11-19 18:43:04 +0400

    changes on the back done and styling needs implementing

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,16 +1,33 @@
-let express = require('express');
-let app = express();
+// let express = require('express');
+// let app = express();
 
-let http = require('http');
-let server = http.Server(app);
+// let http = require('http');
+// let server = http.Server(app);
 
-let socketIO = require('socket.io');
-let io = socketIO(server);
+// let socketIO = require('socket.io');
+// let io = socketIO(server);
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
-  console.log(`Started on port: ${port}`);
+// server.listen(port, () => {
+//   console.log(`Started on port: ${port}`);
+// });
+
+// io.on('connection', (socket) => {
+//   socket.on('join', data => {
+//     socket.join(data.room);
+//     socket.broadcast.to(data.room).emit("user joined")
+//   });
+
+//   socket.on('message', (data) => {
+//     io.in(data.room).emit('new message', { user: data.user, message: data.message })
+//   })
+// })
+
+
+const http = require('http').createServer();
+const io = require('socket.io')(http, {
+  cors: { origin: '*' }
 });
 
 io.on('connection', (socket) => {
@@ -23,3 +40,5 @@ io.on('connection', (socket) => {
     io.in(data.room).emit('new message', { user: data.user, message: data.message })
   })
 })
+
+http.listen(3000, () => console.log('listening on http://localhost:3000'))
